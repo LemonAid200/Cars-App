a9515072ff18f97b3f44fb5b75d988e16c10056f
Ignatev Vsevolod <ignatevvsevolod1998@gmail.com>
2024-07-06 18:47:04 +0500

    CarView car layout created

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/post">Post</router-link> |
-    <router-link to="/car">Car</router-link>
+    <router-link to="/car">Car</router-link> |
+    <router-link to="/post">Post</router-link>    
   </nav>
   <router-view/>
 </template>
---
+++ src/components/UserTag.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="user_head">
+        <img :src="imgUrl" alt="avatar">
+        <div class="user_head__name"><b>{{ userName }}</b></div>
+        <div class="user_head__carName">{{ carName }}</div>
+    </div>
+</template>
+<script>
+    export default {
+        name:'UserTag',
+        props: {
+            imgUrl: String,
+            userName: String,
+            carName: String
+        }
+    }
+</script>
+
+<style scooped>
+  .user_head {
+    display: grid;
+    grid-template-columns: 70px 1fr;
+    justify-content: start;
+    text-align: left;
+
+  }
+  .user_head img {
+    height: 50px;
+    width: 50px;
+    grid-column: 1 / 1;
+    grid-row: 1 / 3;
+  }
+  .user_head__name {
+    
+  }
+  .user_head__carName {
+    color: rgb(165, 165, 165);
+  }
+</style>
